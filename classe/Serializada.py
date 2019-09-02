c7bae4a1368896254f1d04d945c2d304418ad03a
Valdeilton de Souza <javaibotirama@gmail.com>
2019-09-02 17:49:33 -0300

    Arquivos de teste
    		# arquivos de texte para inserção em arquivo txt

--- classe/Serializada.py
+++ classe/Serializada.py
@@ -1,3 +1,4 @@
+import ast
 class Serializada:
     
     def rum(self,rum):
@@ -6,10 +7,27 @@ class Serializada:
         """
         return rum;
     
-    def insertTxt(self,dadosIsert):
+    def insertTxt(self,dadosIsert,nomeArquivo,option):
         """
         Methodo que insere os dados no arquivo txt
         """
+
+        if option==1:
+            try:
+                with open(nomeArquivo, 'a') as f:
+                     let=open(nomeArquivo,"r");
+
+                     if sum(1 for linha in let)==0:
+                         f.write(dadosIsert);
+                         f.close;
+                     else:
+                         f.write("\n"+dadosIsert);
+                         f.close;
+
+                   
+            except IOError:
+                print (u'ERRO: Arquivo não encontrado!')
+
         return dadosIsert;
 
     def updateTxt(self,dadosUpdate):
@@ -24,7 +42,46 @@ class Serializada:
         """
         return dadosDelet;
     
-    def selectTxt(self,dadosDelet):
+    def selectTxt(self,nomeArquivo,optinon,values="",where=""):
+        result="";
         """Methodo que faz o select no arquivo txt e retorna os dados
         """
-        return dadosDelet;
+        try:
+            with open(nomeArquivo, 'r') as f:
+                 if where=="":
+                     result=f.readlines();
+
+                 if where !="" and values !="":
+
+                    re=f.readlines();
+                    for i in re:
+                        valor=ast.literal_eval(i)
+                        if valor[where]==values:
+                             result={"id":str(valor["id"]),"senha":valor["senha"],"login":valor["login"]}
+                             break;                
+                
+                 else:
+                     print("Termos de busca não preenchido para essa função")
+                 
+        except IOError:
+                print (u'ERRO: Arquivo não encontrado!')
+                        
+        return result;
+
+    def convertList(self,lista):
+        """
+          Metodo para converte uma lista em string
+        """
+        s = [str(i) for i in lista] 
+        res = ",".join(s);
+        return res;
+    def sequence(self,nomeArquivo):
+        
+         try:
+                with open(nomeArquivo, 'r') as f:
+                    result = f.readlines().__len__();
+                    print(result)
+         except IOError:
+                print (u'ERRO: Arquivo não encontrado!')
+
+         return result+1;
--- initmain.py
+++ initmain.py
@@ -1,11 +1,22 @@
 from classe import Serializada
+import os
+
 if __name__=="__main__":
     print("dentro do main")
-    
+
     serial=Serializada.Serializada();
     initProgram=serial.rum(True)
+
     while initProgram :
-         interupt=input("Digite 1 para interromper o programa");
-         if interupt=="1":
-             initProgram=serial.rum(False);
+          nomeArquivo="arquivobanco/conta.txt"
+          interupt=input("Digite 1 para interromper o programa");
+
+          dados=[{"id":serial.sequence(nomeArquivo),"senha":"@cabral","login":'indigina'}];
+          
+          if interupt=="1":
+                serial.insertTxt(serial.convertList(dados),nomeArquivo,1)
+                print(serial.selectTxt(nomeArquivo,1,"@90BOBSENHA","senha"));
+                for wr in serial.selectTxt(nomeArquivo,1,"122","senha"):
+                   # print("result",wr)
+                initProgram=serial.rum(False);
 
